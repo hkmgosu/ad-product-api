e599ade21d8442c9aacf6642638017df37c3f5e0
Erick Quiroz <erick.quiroz@live.com>
2025-01-17 13:43:28 -0300

    mongo memory server for e2e fix

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,14 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 describe('App E2E Tests', () => {
   let app: INestApplication;
+  let mongoServer: MongoMemoryServer;
 
   beforeAll(async () => {
-    jest.setTimeout(20000); // Set timeout for this hook
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [MongooseModule.forRoot(uri), AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -17,7 +21,7 @@ describe('App E2E Tests', () => {
   });
 
   afterAll(async () => {
-    jest.setTimeout(10000);
+    await mongoServer.stop();
     await app.close();
   });
 
